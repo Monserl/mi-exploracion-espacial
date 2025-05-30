3b9e26b98f4f292cfecc10d0cbaafcb6bc8189b3
Karen Monserrat <karen1.fresita02@gmail.com>
2025-05-30 13:10:04 -0600

    archivo planetas.js

---
+++ planetas.js
@@ -0,0 +1,45 @@
+const planetas = [
+    {
+      nombre: "Titán",
+      descripcion: "La luna más grande de Saturno, con lagos de metano.",
+      descubiertoEn: "1655",
+      distanciaTierra: "1.2 billones de km",
+      tipo: "Luna",
+      imagen: "🪐"
+    },
+    {
+      nombre: "Próxima Centauri b",
+      descripcion: "Un exoplaneta rocoso en la zona habitable de su estrella.",
+      descubiertoEn: "2016",
+      distanciaTierra: "4.24 años luz",
+      tipo: "Exoplaneta",
+      imagen: "🌟"
+    },
+    {
+      nombre: "Marte",
+      descripcion: "El planeta rojo, objetivo de futuras misiones humanas.",
+      descubiertoEn: "Antigüedad",
+      distanciaTierra: "54.6 millones de km",
+      tipo: "Planeta",
+      imagen: "🔴"
+    },
+    {
+      nombre: "Kepler-438b",
+      descripcion: "Planeta similar a la Tierra orbitando una estrella enana roja.",
+      descubiertoEn: "2015",
+      distanciaTierra: "470 años luz",
+      tipo: "Exoplaneta",
+      imagen: "🌍"
+    },
+    {
+        nombre: "Júpiter",
+        descripcion: "El planeta más grande del sistema solar",
+        descubiertoEn: "Antigüedad",
+        distanciaTierra: "588 millones de km",
+        tipo: "Planeta gigante gaseoso",
+        imagen: "🪐"
+    }
+  ];
+  
+  export default planetas;
+
